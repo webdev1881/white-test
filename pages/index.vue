afe33bcfc2e85fcca4651b2426557a08e473e540
webdev2000923 <webdev1881@gmail.com>
2024-10-22 18:45:43 +0300

    add tailwind

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class='test text-red-400'>
+        test
+    </div>
+
+</template>
+
+
+<script>
+
+export default {
+    name: 'test',
+    data: () => ({
+        
+    }),
+}
+
+</script>
+
+
+
+<style lang='scss' scoped>
+    
+    
+    
+</style>
