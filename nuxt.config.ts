22f9a3c3f36bbf777f93463e8c413450e609869f
webdev2000923 <webdev1881@gmail.com>
2024-10-22 19:04:14 +0300

    refresh date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-10-22',
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint', 
