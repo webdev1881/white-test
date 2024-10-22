5bfee8e002b7bde0c65d2618a740d77186a994bb
webdev2000923 <webdev1881@gmail.com>
2024-10-22 18:26:49 +0300

    add pinia + eslint congig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: [
+    '@nuxt/eslint',
+
+    '@pinia/nuxt',
+  ],
+  devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+
 })
